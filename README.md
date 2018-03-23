cc43c2dc70d46a2f77a1767dd2f22fa88b506219
dankuck <dankuck@gmail.com>
2018-03-22 23:29:12 -0400

    It sorts the schedule.

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 A solution to the <a href="https://github.com/groveco/grove-scheduler-challenge
 ">Grove Scheduler Challenge</a> from Grove Collaborative
 
+# Running the Scheduler
+
+You can run the scheduler by visiting <a href="https://dankuck.github.io/grove-scheduler-challenge/">https://dankuck.github.io/grove-scheduler-challenge/</a>.
+
 # Installation
 
-If you'd like to install this yourself instead of using the supplied compiled version, you can follow these steps:
+If you'd like to install this yourself instead of using the live version above, you can follow these steps:
 
 * Checkout this repository
 * `npm install`
--- src/Scheduler.vue
+++ src/Scheduler.vue
@@ -5,7 +5,7 @@
         </div>
         <div v-else-if="schedules" class="col-lg-12">
             <ul>
-                <li v-for="schedule in schedules">
+                <li v-for="schedule in sortedSchedules">
                     {{ schedule.attributes.name }} on {{ schedule.next }}
                 </li>
             </ul>
@@ -31,22 +31,46 @@ export default {
         setInterval(() => {
             this.checkSchedules();
         }, 500);
-        this.$http.get(this.scheduleEndpoint)
-            .then(
-                response => {
-                    let schedules = response.data.data;
-                    schedules
-                        .forEach(schedule => {
-                            schedule.cronInterval = cronparser.parseExpression(schedule.attributes.cron);
-                        });
-                    this.schedules = schedules;
-                    this.checkSchedules();
-                },
-                response => this.error = true
-            );
+        this.loadSchedules();
+    },
+    computed: {
+        sortedSchedules() {
+            if (!this.schedules) {
+                return [];
+            }
+            let schedules = this.schedules.slice(); // aka copy
+            schedules.sort((a, b) => {
+                if (a.next.toDate() < b.next.toDate()) {
+                    return -1;
+                } else if (a.next.toDate() > b.next.toDate()) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
+            return schedules;
+        },
     },
     methods: {
+        loadSchedules() {
+            this.$http.get(this.scheduleEndpoint)
+                .then(
+                    response => {
+                        let schedules = response.data.data;
+                        schedules
+                            .forEach(schedule => {
+                                schedule.cronInterval = cronparser.parseExpression(schedule.attributes.cron);
+                            });
+                        this.schedules = schedules;
+                        this.checkSchedules();
+                    },
+                    response => this.error = true
+                );
+        },
         checkSchedules() {
+            if (!this.schedules) {
+                return;
+            }
             this.schedules
                 .forEach(schedule => {
                     schedule.next = schedule.cronInterval.next();
